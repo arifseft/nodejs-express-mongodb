589a4eac38c91f1ff3d37ef6f187878f7351a823
M. Arif Sefrianto <arifsefrianto@gmail.com>
2020-05-29 17:48:08 +0700

    adjust

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const expressLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
 
-app.set("view-engine", "ejs");
-app.set("views", __dirname, "/views");
-app.set("layout", "/layouts/layout");
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "./layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
